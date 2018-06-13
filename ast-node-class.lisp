3dd5615d56100be1073d770444907c29f87e2754
matol004 <mattias.olsson.1@hotmail.com>
2018-06-13 22:29:15 +0200

    Start to add data slots to the ast nods

--- ast-node-class.lisp
+++ ast-node-class.lisp
@@ -1,7 +1,10 @@
 (in-package :ast-node-space)
 
 (defclass ast-node-base-class ()
-  ((parsed-node  :accessor parsed-node)))
+  ((symbol
+    :initarg :symbol
+    :reader symbol)
+   (data :accessor data)))
 (defgeneric is-node-type? (node tokenizer node-stack)
   (:documentation "Analyses the state and determines if it is a node of this type."))
 (defgeneric make-node (node tokenzier node-stack))
